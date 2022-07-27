e25a2f823783f0e821d3bc9acc0eb957fedd53a5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-27 16:00:44 +0300

    add initial components/layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,17 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   useEffect(() => { setIsSSR(false); }, [])
   if (isSSR) return null;
 
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      Navbar
+      <div className='flex gap-6 md:gap-20'>
+        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>Sidebar</div>
+        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh[ videos flex-1'>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MyApp;
