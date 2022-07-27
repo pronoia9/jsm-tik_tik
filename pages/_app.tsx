eec522d9a2f40ed299f00eeab8554a78bcff4197
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-27 16:34:06 +0300

    add init app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,20 +8,22 @@ import '../styles/globals.css';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // server side rendering check, only render if its client side
   const [isSSR, setIsSSR] = useState(true);
-  useEffect(() => { setIsSSR(false); }, [])
+  useEffect(() => { setIsSSR(false); }, []);
   if (isSSR) return null;
 
   return (
-    <div>
+    <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
       <Navbar />
-      <div className='flex gap-6 md:gap-20'>
-        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'><Sidebar /></div>
-        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh[ videos flex-1'>
+      <div className='flex gap-6 md:gap-20 '>
+        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+          <Sidebar />
+        </div>
+        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
           <Component {...pageProps} />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default MyApp;
