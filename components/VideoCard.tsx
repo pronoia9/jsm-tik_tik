d4b1aaceab7a1c5298787da19a6c0db1984d7a3c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-29 22:15:11 +0300

    add hover state

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -16,7 +16,7 @@ interface IProps {
 const VideoCard: NextPage<IProps> = ({ post: { caption, postedBy, video, _id, likes } }) => {
   // const VideoCard: NextPage<IProps> = ({ post: { caption, postedBy, video, _id, likes }, isShowingOnHome }) => {
   // const [playing, setPlaying] = useState(false);
-  // const [isHover, setIsHover] = useState(false);
+  const [isHover, setIsHover] = useState(false);
   // const [isVideoMuted, setIsVideoMuted] = useState(false);
   // const videoRef = useRef<HTMLVideoElement>(null);
 
@@ -86,7 +86,7 @@ const VideoCard: NextPage<IProps> = ({ post: { caption, postedBy, video, _id, li
       </div>
 
       <div className='lg:ml-20 flex gap-4 relative'>
-        <div onMouseEnter={() => 'setIsHover(true)'} onMouseLeave={() => 'setIsHover(false)'} className='rounded-3xl'>
+        <div onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} className='rounded-3xl'>
           <Link href={`/detail/${_id}`}>
             <video
               className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[528px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
@@ -95,6 +95,7 @@ const VideoCard: NextPage<IProps> = ({ post: { caption, postedBy, video, _id, li
               src={video.asset.url}></video>
           </Link>
 
+          {/* Playback buttons */}
           {/* {isHover && (
             <div className='absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] lg:w-[600px] p-3'>
               {playing ? (
