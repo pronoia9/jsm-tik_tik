52a592fd4812c4cd2fd84d5aba691782c77d1341
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-29 17:51:12 +0300

    add initial home button

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -15,25 +15,24 @@ const Sidebar: NextPage = () => {
   // const { pathname } = useRouter();
   // const { fetchAllUsers, allUsers }: any = useAuthStore();
 
-  // const activeLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded', normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold rounded';
-
   return (
     <div id='sidebar-component'>
       <div className='block xl:hidden m-2 ml-4 mt-3 text-xl' onClick={() => setShowSidebar((prev) => !prev)}>
         {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
       {showSidebar && (
-        <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3 '>
-          {/* <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
+        <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
+          <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
             <Link href='/'>
-              <div className={pathname === '/' ? activeLink : normalLink}>
+              <div
+                className={`flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold rounded  text-[#F51997]` /* ${pathname === '/' ? ' text-[#F51997]' : ''} */}>
                 <p className='text-2xl'>
                   <AiFillHome />
                 </p>
                 <span className='capitalize text-xl hidden xl:block'>For You</span>
               </div>
             </Link>
-          </div> */}
+          </div>
 
           {/* <Discover />
           <SuggestedAccounts fetchAllUsers={fetchAllUsers} allUsers={allUsers} />
