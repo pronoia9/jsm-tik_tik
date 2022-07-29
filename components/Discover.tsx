d72d9f0e92a8c1a87abe2ecdabd02924502b9704
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-29 20:06:57 +0300

    add dynamic class for active topic

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { NextPage } from 'next';
 import Link from 'next/link';
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 
 import { topics } from '../utils/constants';
 
 const Discover: NextPage = () => {
-  // const router = useRouter();
-  // const { topic } = router.query;
+  const router = useRouter(), { topic } = router.query;
 
   return (
     <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
@@ -16,7 +15,7 @@ const Discover: NextPage = () => {
         {topics?.map((item) => (
           <Link href={`/?topic=${item.name}`} key={item.name}>
             <div
-              className={`xl:border-2 hover:bg-primary px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer ${'topic' === item.name ? 'xl:border-[#F51997] text-[#F51997]' : 'xl:border-gray-300 text-black'}`}>
+              className={`xl:border-2 hover:bg-primary px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer ${topic === item.name ? 'xl:border-[#F51997] text-[#F51997]' : 'xl:border-gray-300 text-black'}`}>
               <span className='font-bold text-2xl xl:text-md '>{item.icon}</span>
               <span className={`font-medium text-md hidden xl:block capitalize`}>{item.name}</span>
             </div>
