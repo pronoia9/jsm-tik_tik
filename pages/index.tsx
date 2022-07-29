460adfc040015b0bb146df75444dd9caff10ab5e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-29 21:40:29 +0300

    comment out a prop

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home = ({ videos }: IProps) => {
   return (
     <div className='flex flex-col gap-10 videos h-full'>
       {videos.length ? (
-        videos?.map((video: Video) => <VideoCard post={video} isShowingOnHome key={video._id} />)
+        videos?.map((video: Video) => <VideoCard post={video} /* isShowingOnHome */ key={video._id} />)
       ) : (
         <NoResults text={`No Videos`} />
       )}
