1bab2449126480ed3b1a0989a172f8f01665d31f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-30 19:49:30 +0300

    remove google login

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
