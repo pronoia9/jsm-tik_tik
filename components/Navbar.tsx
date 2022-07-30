e8a9de5f1137439d3413ff83883376370140c747
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-30 21:57:58 +0300

    add initial google login button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
@@ -29,16 +30,21 @@ const Navbar = () => {
   //   }
   // };
 
+  // !TEMP
+  let user = false;
+
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+      {/* Logo / homepage link */}
       <Link href='/'>
         <div className='w-[100px] md:w-[130px] md:h-[30px] h-[38px]'>
           <Image className='cursor-pointer' src={Logo} alt='logo' layout='responsive' />
         </div>
       </Link>
 
-      {/* <div className='relative hidden md:block'>
-        <form onSubmit={handleSearch} className='absolute md:static top-10 -left-20 bg-white'>
+      {/* Search bar / form */}
+      <div className='relative hidden md:block'>
+        {/* <form onSubmit={handleSearch} className='absolute md:static top-10 -left-20 bg-white'>
           <input
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
@@ -50,12 +56,14 @@ const Navbar = () => {
             className='absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400'>
             <BiSearch />
           </button>
-        </form>
+        </form> */}
       </div>
+
+      {/* User account */}
       <div>
         {user ? (
           <div className='flex gap-5 md:gap-10'>
-            <Link href='/upload'>
+            {/* <Link href='/upload'>
               <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
                 <IoMdAdd className='text-xl' /> <span className='hidden md:block'>Upload </span>
               </button>
@@ -75,12 +83,15 @@ const Navbar = () => {
                 removeUser();
               }}>
               <AiOutlineLogout color='red' fontSize={21} />
-            </button>
+            </button> */}
           </div>
         ) : (
-          <GoogleLogin onSuccess={(response) => createOrGetUser(response, addUser)} onError={() => console.log('Login Failed')} />
+          <GoogleLogin
+            onSuccess={(response) => console.log(response) /*createOrGetUser(response, addUser)*/}
+            onError={() => console.log('Login Failed')}
+          />
         )}
-      </div> */}
+      </div>
     </div>
   );
 };
