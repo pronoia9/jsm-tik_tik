08f354796711d6118ebbc6135891c79053748a03
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-30 22:26:40 +0300

    get user info and post it for auth api

--- utils/index.ts
+++ utils/index.ts
@@ -3,22 +3,31 @@ import axios from 'axios';
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const createOrGetUser = async (response: any, addUser: any) => {
+  // console.log(response);
   var base64Url = response.credential.split('.')[1];
+  // console.log(base64Url);
   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
-  
-  const { name, picture, sub } = JSON.parse(jsonPayload)
-  
+  // console.log(base64);
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split('')
+      .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+      .join('')
+  );
+  // console.log(jsonPayload);
+
+  const { name, picture, sub } = JSON.parse(jsonPayload);
+
   const user = {
     _id: sub,
     _type: 'user',
     userName: name,
     image: picture,
   };
-  
-  addUser(user);
+  // console.log(user);
 
-  await axios.post(`${BASE_URL}/api/auth`, user);
+  // addUser(user);
+
+  // await axios.post(`${BASE_URL}/api/auth`, user);
+  await axios.post(`http://localhost:3000/api/auth`, user);
 };
