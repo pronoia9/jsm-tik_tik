6c821af5f02026cc3fef7b73afd4f58bae9a0bd8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-31 01:49:00 +0300

    add get func

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,18 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-// import { postDetailQuery } from './../../../utils/queries';
+import { postDetailQuery } from './../../../utils/queries';
 // import { client } from '../../../utils/client';
 // import { uuid } from 'uuidv4';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-    // const { id } = req.query;
-    // const query = postDetailQuery(id);
+    const { id } = req.query;
+    const query = postDetailQuery(id);
 
-    // const data = await client.fetch(query);
+    const data = await client.fetch(query);
 
-    // res.status(200).json(data[0]);
+    res.status(200).json(data[0]);
   } else if (req.method === 'PUT') {
     // const { comment, userId } = req.body;
 
