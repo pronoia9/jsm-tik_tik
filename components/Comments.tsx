9aeabae7620b30515e898d102d3a5d85ff51fc85
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-31 23:39:14 +0300

    format + fix export

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-export const Comments = () => {
-  return (
-    <div>Comments</div>
-  )
-}
+const Comments = () => {
+  return <div>Comments</div>;
+};
+
+export default Comments;
