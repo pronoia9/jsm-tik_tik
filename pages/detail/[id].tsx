5016a03d12c645562c7111ba7ac391f8a94db080
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-31 23:13:57 +0300

    comments

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -79,12 +79,15 @@ const Detail = ({ postDetails }: IProps) => {
       {'post' && (
         <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
           <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black-600'>
+            {/* Close/cancel button */}
             <div className='opacity-90 absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
               <p className='cursor-pointer ' onClick={() => 'router.back()'}>
                 <MdOutlineCancel className='text-white text-[35px] hover:opacity-90' />
               </p>
             </div>
+            
             <div className='relative'>
+              {/* Video */}
               <div className='lg:h-[100vh] h-[60vh]'>
                 <video
                   ref={videoRef}
@@ -94,6 +97,7 @@ const Detail = ({ postDetails }: IProps) => {
                   loop></video>
               </div>
 
+              {/* Play video button  */}
               <div className='absolute top-[45%] left-[40%] cursor-pointer'>
                 {!isPlaying && (
                   <button onClick={onVideoClick}>
