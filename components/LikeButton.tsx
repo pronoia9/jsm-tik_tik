2c7b70d69d6c4b2c4b0453d0eee0ad1385daf7e8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-31 23:39:09 +0300

    format

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const LikeButton = () => {
-  return (
-    <div>LikeButton</div>
-  )
-}
+  return <div>LikeButton</div>;
+};
 
-export default LikeButton
+export default LikeButton;
