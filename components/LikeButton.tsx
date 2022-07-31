071bcb63d4eb40013217415c59e30529026ca743
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-31 23:53:13 +0300

    add handleLike func

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -4,14 +4,14 @@ import { MdFavorite } from 'react-icons/md';
 
 import useAuthStore from '../store/authStore';
 
-// interface IProps {
-//   likes: any;
-//   flex: string;
-//   handleLike: () => void;
-//   handleDislike: () => void;
-// }
+interface IProps {
+  // likes: any;
+  // flex: string;
+  handleLike: () => void;
+  handleDislike: () => void;
+}
 
-const LikeButton: NextPage = () => {
+const LikeButton: NextPage<IProps> = ({ handleLike, handleDislike }) => {
   // const LikeButton: NextPage<IProps> = ({ likes, flex, handleLike, handleDislike }) => {
   const [alreadyLiked, setAlreadyLiked] = useState(false);
   const { userProfile }: any = useAuthStore();
