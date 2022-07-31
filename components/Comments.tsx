a108e59400706280ebddda206ab6de94c2a899e3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-01 01:23:34 +0300

    add initial comment form

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -23,8 +23,8 @@ import NoResults from './NoResults';
 // }
 
 const Comments = () => {
+  const { allUsers, userProfile }: any = useAuthStore();
 // const Comments = ({ comment, setComment, addComment, comments, isPostingComment }: IProps) => {
-  // const { allUsers, userProfile }: any = useAuthStore();
 
   // !TEMP
   const comments = [];
@@ -70,21 +70,25 @@ const Comments = () => {
           <NoResults text='No Comments Yet! Be First to do add the comment.' />
         )}
       </div>
-      {/* {userProfile && (
+      {userProfile && (
         <div className='absolute bottom-0 left-0  pb-6 px-2 md:px-10 '>
-          <form onSubmit={addComment} className='flex gap-4'>
+          <form
+            // onSubmit={addComment}
+            className='flex gap-4'>
             <input
-              value={comment}
-              onChange={(e) => setComment(e.target.value.trim())}
+              // value={comment}
+              onChange={(e) => 'setComment(e.target.value.trim())'}
               className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg'
               placeholder='Add comment..'
             />
-            <button className='text-md text-gray-400 ' onClick={addComment}>
-              {isPostingComment ? 'Commenting...' : 'Comment'}
+            <button className='text-md text-gray-400 '
+              // onClick={addComment}
+            >
+              {'isPostingComment' ? 'Commenting...' : 'Comment'}
             </button>
           </form>
         </div>
-      )} */}
+      )}
     </div>
   );
 };
