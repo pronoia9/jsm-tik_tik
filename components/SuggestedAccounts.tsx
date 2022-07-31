1e298cb8537b9a34208b5f0aec8baaebf8aa02bf
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-01 02:47:40 +0300

    add Link layout

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -25,35 +25,29 @@ const SuggestedAccounts = () => {
     <div className='xl:border-b-2 border-gray-200 pb-4'>
       <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>Suggested accounts</p>
       <div>
-        {/* {users?.slice(0, 6).map((user: IUser) => (
+        {allUsers?.slice(0, 6).map((user: IUser) => (
           <Link href={`/profile/${user._id}`} key={user._id}>
             <div className='flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded'>
               <div className='w-8 h-8'>
-                <Image
+                {/* <Image
                   width={34}
                   height={34}
                   className='rounded-full'
                   src={user.image}
                   alt='user-profile'
                   layout='responsive'
-                />
+                /> */}
               </div>
 
               <div className='hidden xl:block'>
                 <p className='flex gap-1 items-center text-md font-bold text-primary lowercase'>
-                  {user.userName.replace(/\s+/g, '')} <GoVerified className='text-blue-400' />
+                  {/* {user.userName.replace(/\s+/g, '')} <GoVerified className='text-blue-400' /> */}
                 </p>
-                <p className='capitalize text-gray-400 text-xs'>{user.userName}</p>
+                {/* <p className='capitalize text-gray-400 text-xs'>{user.userName}</p> */}
               </div>
             </div>
           </Link>
-        ))} */}
-        {
-          // !TEMP
-          allUsers.slice(0, 6).map((user : IUser) => (
-            <Link href={`/profile/${user._id}`} key={user._id}></Link>
-          ))
-        }
+        ))}
       </div>
     </div>
   );
