0ca0bf33471bb0dd7ea716141c261b46a3691279
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-01 05:08:10 +0300

    add accounts + videos tabs

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -12,25 +12,25 @@ import { BASE_URL } from '../../utils';
 import { IUser, Video } from '../../types';
 
 const Search = ({ videos }: { videos: Video[] }) => {
-  // const [isAccounts, setIsAccounts] = useState(false);
+  const [isAccounts, setIsAccounts] = useState(false);
   // const { allUsers }: { allUsers: IUser[] } = useAuthStore();
 
   // const router = useRouter();
   // const { searchTerm }: any = router.query;
 
-  // const accounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
-  // const isVideos = !isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
+  const accounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
+  const isVideos = !isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
   // const searchedAccounts = allUsers?.filter((user: IUser) => user.userName.toLowerCase().includes(searchTerm));
 
   return (
     <div className='w-full  '>
       <div className='flex gap-10 mb-10 border-b-2 border-gray-200 md:fixed z-50 bg-white w-full'>
-        {/* <p onClick={() => setIsAccounts(true)} className={`text-xl  font-semibold cursor-pointer ${accounts} mt-2`}>
+        <p onClick={() => setIsAccounts(true)} className={`text-xl  font-semibold cursor-pointer ${accounts} mt-2`}>
           Accounts
-        </p> */}
-        {/* <p className={`text-xl font-semibold cursor-pointer ${isVideos} mt-2`} onClick={() => setIsAccounts(false)}>
+        </p>
+        <p className={`text-xl font-semibold cursor-pointer ${isVideos} mt-2`} onClick={() => setIsAccounts(false)}>
           Videos
-        </p> */}
+        </p>
       </div>
       {/* {isAccounts ? (
         <div className='md:mt-16'>
