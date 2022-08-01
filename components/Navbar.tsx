b982a000bb2b48ea1807bf1af8ad69dee6bb319b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-01 04:51:20 +0300

    add initial input

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -42,17 +42,17 @@ const Navbar = () => {
       {/* Search bar / form */}
       <div className='relative hidden md:block'>
         <form onSubmit={handleSearch} className='absolute md:static top-10 -left-20 bg-white'>
-          {/* <input
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
+          <input
+            // value={searchValue}
+            // onChange={(e) => setSearchValue(e.target.value)}
             className='bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0'
             placeholder='Search accounts and videos'
           />
           <button
-            onClick={handleSearch}
+            // onClick={handleSearch}
             className='absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400'>
             <BiSearch />
-          </button> */}
+          </button>
         </form>
       </div>
 
